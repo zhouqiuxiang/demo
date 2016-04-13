6aff64269d41f32ed26a787d84988ce64fc676d7
zhouqiuxiang <3102274477@qq.com>
2016-04-13 15:42:46 +0800

    new test by xiang

---
+++ src/TestNG/FirstTest.java
@@ -0,0 +1,22 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class FirstTest {
+  @Test
+  public void f() {
+	  System.out.print("@Test");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.print("@BeforeTest");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.print("@AfterTest");
+  }
+
+}
